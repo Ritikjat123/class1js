47a7809e9dc511f2d08ef9ddd3a05b0f73aa6488
Ritik Jat <ritikdudee00@gmail.com>
2025-08-26 22:13:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+//keywords-: Its reserved word which can not use for variable/ function/class naming
+//variable-: Its a container or dabba which hold some value
+//there are 3 way to define variable
+//rules of creating variable/function/class
+//variable are always with alphabets and underscore
+let x=10;
+console.log(x);
+x=90
+console.log(x);
