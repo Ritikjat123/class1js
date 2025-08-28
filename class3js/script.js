6b847cf90dd8d7f083f65125a7a7d6cc6b2454ef
Ritik Jat <ritikdudee00@gmail.com>
2025-08-28 23:12:40 +0530

    Add files via upload

---
+++ class3js/script.js
@@ -0,0 +1,40 @@
+// let obj={
+//     sname:"jagmohan",
+//     age:"26",
+//     email:"jg@gmail.com",
+//     isExperenced:true,
+//     address:"katni",
+//     "e id":"1001",
+//     101:"ritik",
+//     profession:fuction(){
+//         console.log("teching")
+//     }
+
+// }
+// console.log(obj.sname)
+
+
+
+
+
+
+
+ console.log(obj.address)
+// console.log(obj["e id"])
+// console.log(obj[101])
+// console.log(obj.profession)
+// console.log(obj.profession())
+
+// let obj1={
+//    s2name:"vijay",//    emails:"1212@gmail.com",
+//    value:function(){
+//     //   const name={toname:"89990"}
+//       return value();
+//      }
+//    }
+ 
+// let op=obj1.value();
+// console.log(op)
+
+let arr=[12,34,56,78,101];
+console.log(arr)
