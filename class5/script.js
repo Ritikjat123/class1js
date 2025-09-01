ad88b19ceb57d4dc1011b1b6e10203432438b7f0
Ritik Jat <ritikdudee00@gmail.com>
2025-09-01 23:13:22 +0530

    Add files via upload

---
+++ class5/script.js
@@ -0,0 +1,35 @@
+// console.log("hii") // freeze and seal method in obj
+
+// const obj={
+//    fname:"ritik",
+//    age:22
+// }
+// obj.city="harda";
+// console.log("original name is",obj)
+// Object.freeze(obj);
+// obj.city="indore";
+// console.log("original name is",obj)
+                                               //
+// function sum(a,b,...rest){
+//     console.log(rest)
+//     return a+b;
+    
+// }
+// console.log(sum(22,44,22,345,678))
+
+//shallow copy
+// let person = {
+//   name: "Ritik",
+//   address: { city: "Indore", pin: 452001 }
+// };
+
+// // Shallow copy using spread
+// let copy = { ...person };
+
+// copy.name = "Aman"; // change at 1st level
+// copy.address.city = "Bhopal"; // change in nested object
+
+// console.log(person.name);   // Ritik ✅ (independent)
+// console.log(person.address.city); // Bhopal ❌ (linked)
+
+
