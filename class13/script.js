6cc629b1cc55734be68f1f6f4b78d6e8ce5c1d72
Ritik Jat <ritikdudee00@gmail.com>
2025-09-10 23:10:46 +0530

    Add files via upload

---
+++ class13/script.js
@@ -0,0 +1,7 @@
+// console.log("hello");
+// const h1Ele=document.getElementsByTagName("h1");
+// console.log("h1Ele",h1Ele[13]);
+// h1Ele[1].style.backgroundColor="red";
+const ele1=document.createElement("p");
+ele1.innerContent="this is span tag";
+console.log(ele1);
